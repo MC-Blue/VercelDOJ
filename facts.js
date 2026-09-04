8d02bba78abf9a0c2a2f6d1983ea4dad0ecd1179
Blue <romanambro43@gmail.com>
2026-09-04 18:26:27 +0200

    Add Ammunation

--- facts.js
+++ facts.js
@@ -132,7 +132,7 @@ Blanchiment d'argent;500000;25;Délit majeur;
 Faux témoignage;500000;20;Délit majeur;
 Braquage de coiffeur;150000;20;Délit mineur;
 Complicité de Braquage de coiffeur;150000;20;Délit mineur;
-Transport de marchandises illégales;350000;15;Délit majeur;
+Braquage d'Ammunation;150000;15;Délit mineur;
 `;
 
   const nameCollator = new Intl.Collator("fr", {
